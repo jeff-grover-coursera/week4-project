ed3bc67d3416f7c1b4d19e5bd429d115447b19bd
Jeff Grover <jeffrey.grover13@gmail.com>
2016-10-29 15:19:12 -0500

    Wrote data import and cleaning code in plot1.R

---
+++ plot1.R
@@ -0,0 +1,22 @@
+# Import data
+wd <- "/Users/jeffgrover/Dropbox/Coursera/4 Exploratory Data Analysis/week4-project"
+setwd(wd)
+NEI <- readRDS(paste0(wd,"/exdata%2Fdata%2FNEI_data/summarySCC_PM25.rds"))
+SCC <- readRDS(paste0(wd,"/exdata%2Fdata%2FNEI_data/Source_Classification_Code.rds"))
+
+
+# Clean data
+## Convert string variables to factors
+NEI$fips <- as.factor(NEI$fips)
+NEI$SCC <- as.factor(NEI$SCC)
+NEI$Pollutant <- as.factor(NEI$Pollutant)
+NEI$type <- as.factor(NEI$type)
+
+## Replace levels of SCC with names?
+#levels(NEI$SCC)
+
+
+# Plot
+# Have total emission from PM2.5 decreased in the US from 1999 to 2008?
+# Use the base plotting system, plot the total PM2.5 emission from all sources
+# for each of the years 1999, 2002, 2005, and 2008.
